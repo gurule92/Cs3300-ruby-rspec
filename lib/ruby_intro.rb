f0c063b0ec71402deaccade549a59354f0af3859
Connor <cgurule@uccs.edu>
2023-02-24 06:22:18 +0000

    part1 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,24 +4,38 @@
 
 def sum arr
   # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  return arr.max(2).sum
+
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  for x in 0..arr.length-1
+    for p in x+1..arr.length-1
+      if arr[x] + arr[p] == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  output = "Hello, "
+  return output.concat(name)
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  
 end
 
 def binary_multiple_of_4? s
